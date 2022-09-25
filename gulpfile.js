7ea6cdf17fe5d1c12179f5d24a3581265dc5f1e9
Devin Blais <devin.blais@opendoor.com>
2022-09-25 14:56:35 -0700

    Add intro animation. Fix gulp compress bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const minify = require('gulp-minify');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
@@ -9,4 +10,11 @@ gulp.task('sass', function() {
 
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+ 
+gulp.task('compress', function() {
+  gulp.src(['js/*.js'])
+    .pipe(minify())
+    .pipe(gulp.dest('min'))
 });
